bc5e0e9407c636f1457e4b48e8259a13ae5fd0ff
dekitsuneco <art199798@mail.ru>
2022-02-12 00:43:17 +0500

    Get content of the specified directory

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const fs = require('fs');
 const { join } = require('path');
+
+const traverse = dir => {
+  /* 
+    Get a content item of the dir and check if it's a directory'.
+    If it is, read already its content.
+  */
+  const subdirs = fs
+    .statSync(dir)
+    .isDirectory()
+    && fs.readdirSync(dir);
+}
+
